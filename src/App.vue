ecd2755f8577efc1ef1f4100d490d99ae6f43403
Brock Wilcox <awwaiid@thelackthereof.org>
2019-05-27 18:07:29 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <Game
+    @hmm="ya"
+  />
+</template>
+
+
+<script>
+import Game from '@/components/Game'
+export default {
+  name: 'App',
+  components: { Game },
+  methods: {
+    ya() {
+      console.log("ya!")
+    }
+  }
+}
+</script>
+
+
+<style lang="scss">
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+}
+</style>
